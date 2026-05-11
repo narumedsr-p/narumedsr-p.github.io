bfd858605127af1ecdc28e463554790319aea9f8
Narumedsr Pitayachamrat <narumedsr.p@gmail.com>
2026-05-11 10:58:23 +0700

    feat: add custom domain nayac08.win

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 // Change `base` if deploying to a project repo (e.g., base: '/my-portfolio')
 // Leave empty if repo is named narumedsr-p.github.io
 export default defineConfig({
-  site: 'https://narumedsr-p.github.io',
+  site: 'https://nayac08.win',
   integrations: [tailwind()],
 });
